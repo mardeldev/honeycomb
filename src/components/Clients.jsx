26c52b395dac6b773561397a5e9a26a28585587c
Aimable Mardel <73969781+aimardel@users.noreply.github.com>
2023-07-18 23:07:58 +0100

    Footer section complete

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -5,7 +5,7 @@ const Client = () => (
     <section className={`${styles.flexCenter} my-4`}>
         <div className={`${styles.flexCenter} flex-wrap w-full`}>
             {clients.map((client) => (
-                <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
+                <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] h-[100px] rounded-[50px] masking`}>
                     <img src={client.logo} alt="client_logo" className="sm:w-[192px] w-[100px] object-contain" />
                 </div>
             ))}
@@ -15,3 +15,4 @@ const Client = () => (
 )
 
 export default Client
+
